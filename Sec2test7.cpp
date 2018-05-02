4fa4ddefe121aa6bc645e1bf0566444363090b22
lishikuan043 <417788988@qq.com>
2018-05-02 21:51:11 +0800

    add some tests.

---
+++ Sec2test7.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Administrator on 2018/5/2.
+//
+#include <iostream>
+using namespace std;
+
+void fun(int h,int m) {
+    if((h>=0 && h<=23 && m>=0 && m<=59)||(h==24 && m==0)) {
+        cout << "Time: " << h << ":" << m <<endl;
+    } else{
+        cout << "Wrong time!"<<endl;
+    }
+}
+
+int main()
+{
+    int h,m;
+    cout << "Enter the number of hours: ";
+    cin >> h;
+    cout << "Enter the number of minutes: ";
+    cin >> m;
+    fun(h,m);
+
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
+//
+// Created by Administrator on 2018/5/2.
+//
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
