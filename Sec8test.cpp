e80e51800593d46bfd0f5216229424aabb622529
lishikuan043 <417788988@qq.com>
2018-06-18 18:24:23 +0800

    add Sec8test.

--- Sec8test.cpp
+++ Sec8test.cpp
@@ -71,9 +71,64 @@ void test3(){
     }
 }
 
+template <class T>
+void quicksortdemo(T arrs[],int start,int end);
+
+
+template <class T>
+T maxn(T ts[],int n){
+   //int length = sizeof(ts)/ sizeof(ts[0]);
+    //int length = end(ts)- begin(ts);
+    cout << n << endl;
+    quicksortdemo(ts,0,n-1);
+    return ts[n-1];
+}
+
+template <class T>
+void quicksortdemo(T arrs[],int start,int end){
+    //int length = sizeof(arrs)/ sizeof(arrs[0]);
+    T key = arrs[start];
+    int l = start;
+    int h = end;
+    while(l<h){
+        while (l<h && arrs[h]>=key){
+            h--;
+        }
+        if(l<h){
+            arrs[l] = arrs[h];
+            l++;
+        }
+        while(l<h && arrs[l]<=key){
+            l++;
+        }
+        if(l<h){
+            arrs[h] = arrs[l];
+            h--;
+        }
+    }
+    arrs[l] = key;
+    if(l>start){
+        quicksortdemo(arrs,start,l-1);
+    }
+    if(end>l){
+        quicksortdemo(arrs,h+1,end);
+    }
+
+}
+
+void test5(){
+    int ints[] = {1,2,4,5,6};
+    int n1 = sizeof(ints)/ sizeof(ints[0]);
+    double ds[] = {2.2,1.1,5.5,4.4,3.3};
+    int n2 = sizeof(ds)/ sizeof(ds[0]);
+    cout << maxn(ints,n1) << endl;
+    cout << maxn(ds,n2) << endl;
+}
+
 int main(){
     //test1();
     //test2();
     //test3();
+    test5();
     return 0;
 }
